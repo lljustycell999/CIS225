2fc5ea74c0c5311173440d40b2b8fda5e90bec73
lljustycell999 <123667513+lljustycell999@users.noreply.github.com>
2024-01-15 21:03:34 -0500

    Update CIS 225 Laboratory 03 - Midterm Average Calculator.asm

--- CIS 225 Laboratory 03 - Midterm Average Calculator.asm
+++ CIS 225 Laboratory 03 - Midterm Average Calculator.asm
@@ -51,17 +51,17 @@ main proc
 	mov    ecx, 4
 	mov    esi, OFFSET labs
 	mov    edx, OFFSET labMsg
-	call InputGrades
+	call   InputGrades
 
 	mov    ecx, 3
 	mov    esi, OFFSET quizzes
 	mov    edx, OFFSET quizMsg
-	call InputGrades
+	call   InputGrades
 
 	mov    ecx, 2
 	mov    esi, OFFSET homeworks
 	mov    edx, OFFSET homeworkMsg
-	call InputGrades
+	call   InputGrades
 
 	mov    ecx, 1
 	mov    esi, OFFSET exam
@@ -83,160 +83,160 @@ main endp
 
 InputGrades proc
 
-        ;Input grades
-        mov    ax, 0
-lp1:    call   WriteString
-        call   ReadDec
-        mov    [esi], ax
-        add    esi, 2
-        loop   lp1		
-        ret
+	        ;Input grades
+	        mov    ax, 0
+	lp1:    call   WriteString
+        	call   ReadDec
+	        mov    [esi], ax
+	        add    esi, 2
+	        loop   lp1		
+	        ret
 
 InputGrades endp
 
 ComputeAverages proc
 
-	;Total up each grade component and calculate their averages
-
-	;Total lab points
-	mov    ax, 0
-lp2:	add    ax, [esi]
-	add    esi, 2
-	loop   lp2
-
-	;Calculate lab average
-	mov    bx, 4
-	mov    dx, 0
-	div    bx
-	mov    labAvg, ax
-
-	;Total quiz points
-	mov	 ax, 0
-	mov    ecx, 3
-	mov    esi, OFFSET quizzes
-lp3:	add    ax, [esi]
-	add    esi, 2
-	loop   lp3
+		;Total up each grade component and calculate their averages
 	
-	;Calculate quiz average
-	mov    bx, 3
-	mov    dx, 0
-	div    bx
-	mov    quizAvg, ax
-
-	;Total homework points
-	mov    ax, 0
-	mov    ecx, 2
-	mov    esi, OFFSET homeworks
-lp4:	add    ax, [esi]
-	add    esi, 2
-	loop   lp4
-
-	;Calculate homework average
-	mov    bx, 2
-	mov    dx, 0
-	div    bx
-	mov    homeworkAvg, ax
-
-	;Total exam points
-	mov    ecx, 1
-	mov    ax, exam
-
-	;Calculate exam average
-	mov    bx, 100
-	mul    bx
-	mov    bx, 127
-	mov    dx, 0
-	div    bx
-	mov    examAvg, ax
-
-	;Calculate midterm average
-	mov    bx, 25
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	push   dx
-	mov    midtermAvg, ax
-
-	mov    ax, homeworkAvg
-	mov    bx, 10
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	push   dx
-	add    midtermAvg, ax
-
-	mov    ax, quizAvg
-	mov    bx, 10
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	push   dx
-	add    midtermAvg, ax
-
-	mov    ax, labAvg
-	mov    bx, 35
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	push   dx
-	add    midtermAvg, ax
-
-	;Display output with midterm average rounded to two decimal places
-	pop    ax
-	mov    bx, 100
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	mov    cx, ax
-
-	pop    ax
-	mov    bx, 100
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	add    cx, ax
-
-	pop    ax
-	mov    bx, 100
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	add    cx, ax
-
-	pop    ax
-	mov    bx, 100
-	mul    bx
-	mov    bx, 80
-	mov    dx, 0
-	div    bx
-	add    cx, ax
-
-	mov    ax, cx
-	mov    bx, 100
-	mov    dx, 0
-	div    bx
-	push   dx
-	add    midtermAvg, ax
-
-	mov    edx, OFFSET midtermMsg
-	call   WriteString
-	mov    ax, midtermAvg
-	call   WriteDec
-	mov    al, '.'
-	call   WriteChar
-	pop    ax
-	call   WriteDec
-	call   Crlf
-
-	ret 
+		;Total lab points
+		mov    ax, 0
+	lp2:	add    ax, [esi]
+		add    esi, 2
+		loop   lp2
+	
+		;Calculate lab average
+		mov    bx, 4
+		mov    dx, 0
+		div    bx
+		mov    labAvg, ax
+	
+		;Total quiz points
+		mov	 ax, 0
+		mov    ecx, 3
+		mov    esi, OFFSET quizzes
+	lp3:	add    ax, [esi]
+		add    esi, 2
+		loop   lp3
+		
+		;Calculate quiz average
+		mov    bx, 3
+		mov    dx, 0
+		div    bx
+		mov    quizAvg, ax
+	
+		;Total homework points
+		mov    ax, 0
+		mov    ecx, 2
+		mov    esi, OFFSET homeworks
+	lp4:	add    ax, [esi]
+		add    esi, 2
+		loop   lp4
+	
+		;Calculate homework average
+		mov    bx, 2
+		mov    dx, 0
+		div    bx
+		mov    homeworkAvg, ax
+	
+		;Total exam points
+		mov    ecx, 1
+		mov    ax, exam
+	
+		;Calculate exam average
+		mov    bx, 100
+		mul    bx
+		mov    bx, 127
+		mov    dx, 0
+		div    bx
+		mov    examAvg, ax
+	
+		;Calculate midterm average
+		mov    bx, 25
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		push   dx
+		mov    midtermAvg, ax
+	
+		mov    ax, homeworkAvg
+		mov    bx, 10
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		push   dx
+		add    midtermAvg, ax
+	
+		mov    ax, quizAvg
+		mov    bx, 10
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		push   dx
+		add    midtermAvg, ax
+	
+		mov    ax, labAvg
+		mov    bx, 35
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		push   dx
+		add    midtermAvg, ax
+	
+		;Display output with midterm average rounded to two decimal places
+		pop    ax
+		mov    bx, 100
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		mov    cx, ax
+	
+		pop    ax
+		mov    bx, 100
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		add    cx, ax
+	
+		pop    ax
+		mov    bx, 100
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		add    cx, ax
+	
+		pop    ax
+		mov    bx, 100
+		mul    bx
+		mov    bx, 80
+		mov    dx, 0
+		div    bx
+		add    cx, ax
+	
+		mov    ax, cx
+		mov    bx, 100
+		mov    dx, 0
+		div    bx
+		push   dx
+		add    midtermAvg, ax
+	
+		mov    edx, OFFSET midtermMsg
+		call   WriteString
+		mov    ax, midtermAvg
+		call   WriteDec
+		mov    al, '.'
+		call   WriteChar
+		pop    ax
+		call   WriteDec
+		call   Crlf
+	
+		ret 
 ComputeAverages endp
 
 end main
